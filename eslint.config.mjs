1f767803dc6da733394188bfe81d776cbac27ff4
Yu Ling Chen <chenYuLing@hismax.onmicrosoft.com>
2025-06-14 20:07:21 +0800

    fix: improve Docker testing in GitHub Actions workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["src/test/vitest.d.ts"]
-  }
+    ignores: ["src/test/vitest.d.ts"],
+  },
 ];
 
 export default eslintConfig;
